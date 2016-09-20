e32f76fc03b332fee4a50a9ad69455ee006c0ee9
Alex Munda <alex.munda3@gmail.com>
2016-09-20 10:20:24 -0500

    Simple hello app

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,54 @@
 module Main exposing (..)
 
-import Html exposing (text)
+import Html exposing (..)
+import Html.App as Html
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
 
-main : Html.Html a
+
 main =
-    text "hello"
+    Html.beginnerProgram
+        { model = model
+        , view = view
+        , update = update
+        }
+
+
+type alias Model =
+    { name : String }
+
+
+model : Model
+model =
+    Model ""
+
+
+type Msg
+    = Name String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Name name ->
+            { model | name = name }
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "container" ]
+        [ div [ class "row" ]
+            [ input [ class "col-md-4 col-md-offset-4", type' "text", placeholder "Name", onInput Name ] []
+            , welcomeText model
+            ]
+        ]
+
+
+welcomeText : Model -> Html msg
+welcomeText model =
+    h1 [ class "col-md-4 col-md-offset-4" ] [ text (welcomeJskc model.name) ]
+
+
+welcomeJskc : String -> String
+welcomeJskc name =
+    "Welcome to JSKC, " ++ name

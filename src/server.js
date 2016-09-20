6741b492ded5dc90f0e9a62c6ea7a650c9dd0289
Alex Munda <alex.munda3@gmail.com>
2016-09-20 10:59:00 -0500

    Only use server and package.json in this branch

--- src/server.js
+++ src/server.js
@@ -1,14 +1,13 @@
-import rootPath from 'app-root-path';
-import express from 'express';
-
-const {path} = rootPath;
+const express = require('express');
 
 const app = express();
 
-app.use('/dist', express.static(`${path}/dist`));
-
 app.get('/', (req, res) => {
-  return res.sendFile(`${path}/src/index.html`);
+  return res.status(200).send('Yay microservices.');
 });
 
-app.listen(8080, () => console.log('Listening on port 8080'));
+app.get('/api/jskc', (req, res) => {
+  return res.status(200).send('Hello JSKC');
+});
+
+app.listen(1738);
